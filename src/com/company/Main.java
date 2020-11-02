c9a350c1086132eacff0a594291e37ce53932b93
ArturPelenskyi <pelenskyartyr@gmail.com>
2020-11-02 18:10:10 +0200

    Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    User user = new User();
+	    user.setC1();
+	    user.setC2();
+	    user.setC3();
+
+	    user.setT1();
+	    user.setT2();
+	    user.setT3();
+
+	    user.Counting();
+
+
+    }
+}
