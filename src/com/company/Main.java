da7454739d47ae448bc59895970deec7c7409402
ArturPelenskyi <pelenskyartyr@gmail.com>
2020-11-05 19:31:23 +0200

    Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,21 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         InformationGetter informationGetter = new InformationGetter();
-        informationGetter.setName();
-        informationGetter.setAddress();
+        try {
+            informationGetter.setName();
+        }catch (IOException e)
+        {
+            System.out.println("Source doesn't exist");
+        }
+        try {
+            informationGetter.setAddress();
+        }catch (IOException e)
+        {
+            System.out.println("Source doesn't exist");
+        }
+
         System.out.print("Name is "+informationGetter.getName()+"\tAddress: "+informationGetter.getAddress());
 
 
