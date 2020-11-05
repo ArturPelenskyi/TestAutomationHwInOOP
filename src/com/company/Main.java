0d94fe62f5da22ccc254ace0ab4d4ae717089027
ArturPelenskyi <pelenskyartyr@gmail.com>
2020-11-05 19:26:31 +0200

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,18 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input radius");
         System.out.print("radius:");
-        double radius = Double.parseDouble(br.readLine());
+        double radius = 0;
+        try {
+             radius = Double.parseDouble(br.readLine());
+        }catch (IOException e){
+            System.out.println("Source doesn't exist!");
+        }catch (NumberFormatException e){
+            System.out.println("Not a number!");
+        }
 
         CounterOfAreaAndPerimeter counterOfAreaAndPerimeter = new CounterOfAreaAndPerimeter(radius);
         counterOfAreaAndPerimeter.CountArea();
